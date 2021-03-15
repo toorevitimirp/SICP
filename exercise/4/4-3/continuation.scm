13182ece16166bb95e791f288c13d57af4354ca5
toorevitimirp <db1345569965@gmail.com>
2021-03-15 21:46:06 +0800

    continuation call/cc cps

--- exercise/4/4-3/continuation.scm
+++ exercise/4/4-3/continuation.scm
@@ -1,4 +1,4 @@
-;;;continuation和call/cc的基本概念：
+;;;continuation 和 call/cc 的基本概念：
 ;;;exp (+ (* 3 4) 5)
 (define *k* #f)
 
@@ -36,3 +36,129 @@
 
 *k*
 (define x (*k* 5))
+
+
+;;;一些例子
+;;1.
+(let ([x (call/cc (lambda (k) k))])
+  (x (lambda (ignore) "hi")))
+
+((lambda (x)
+   (x (lambda (ignore) "hi")))
+ (call/cc (lambda (k) k)))
+
+;what is k?
+(lambda (v)
+  ((lambda (x)
+     (x (lambda (ignore) "hi")))
+   v))
+
+(lambda (v)
+  (let ((x v))
+    (x (lambda (ignore "hi")))))
+
+((lambda (x)
+   (x (lambda (ignore) "hi")))
+ (lambda (v)
+   ((lambda (x)
+      (x (lambda (ignore) "hi")))
+    v)))
+
+((lambda (v)
+   ((lambda (x)
+      (x (lambda (ignore) "hi")))
+    v))
+ (lambda (ignore) "hi"))
+
+((lambda (x)
+   (x (lambda (ignore) "hi")))
+ (lambda (ignore) "hi"))
+
+((lambda (ignore) "hi")
+ (lambda (ignore) "hi"))
+
+((lambda (ignore) "hi")
+ (lambda (ignore) "hi"))
+
+;;2.
+(((call/cc (lambda (k) k))
+  (lambda (x) x))
+ "hey!")
+
+;what is k?
+(lambda (v)
+  (v (lambda (x) x)))
+
+(((lambda (v)
+    (v (lambda (x) x)))
+  (lambda (x) x))
+ "hey!")
+
+(((lambda (x) x)
+  (lambda (x) x))"hey!")
+
+((lambda (x) x)"hey!")
+
+;;3.
+(define product
+  (lambda (ls)
+    (call/cc
+      (lambda (break)
+        (let f ([ls ls])
+          (cond
+            [(null? ls) 1]
+            [(= (car ls) 0) (break 0)]
+            [else (* (car ls) (f (cdr ls)))]))))))
+
+(define product
+  (lambda (ls)
+    (let f ([ls ls])
+      (cond
+        [(null? ls) 1]
+        [(= (car ls) 0) 0]
+        [else (* (car ls) (f (cdr ls)))]))))
+;what is break?
+(lambda (v)
+  v)
+
+(define product
+  (lambda (ls)
+    ((lambda (break)
+        (let f ([ls ls])
+          (cond
+            [(null? ls) 1]
+            [(= (car ls) 0) (break 0)]
+            [else (* (car ls) (f (cdr ls)))])))
+     (lambda (v) v))))
+
+(define product
+  (lambda (ls)
+    (let f ([ls ls])
+      (cond
+        [(null? ls) 1]
+        [(= (car ls) 0) ((lambda (v) v) 0)]
+        [else (* (car ls) (f (cdr ls)))]))))
+
+;what the difference?
+(define product
+  (lambda (ls)
+    (let f ([ls ls])
+      (cond
+        [(null? ls) 1]
+        [(= (car ls) 0) 0]
+        [else (* (car ls) (f (cdr ls)))]))))
+
+
+;;;cps
+(define fact-cps
+  (lambda (n k)
+    (cond
+      [(zero? n) (k 1)]
+      [else (fact-cps (sub1 n)
+                      (lambda (v)
+                        (k (* v n))))])))
+;think of v as (fact n)
+
+(define fact
+  (lambda (n)
+    (fact-cps n (lambda (v) v))))
